5aaa0bd3df889814f21ef1ea0d440ab141bd106a
Manas Sharma <101457680+manascs897@users.noreply.github.com>
2026-02-11 00:48:09 +0530

    Added files related to Application

---
+++ Email-Application/src/main/java/com/manas/email/EmailApplication.java
@@ -0,0 +1,31 @@
+package com.manas.email;
+
+import com.manas.email.bean.Mail;
+import com.manas.email.services.MailService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class EmailApplication {
+
+	public static void main(String[] args) {
+
+		Mail mail = new Mail();
+		mail.setMailTo(Arrays.asList("abc@gmail.com", "def@gmail.com"));
+		mail.setMailCc(Arrays.asList("ghi@gmail.com"));
+		mail.setMailBcc(Arrays.asList("jkl@gmail.com"));
+		mail.setAttachmentFileName("mno");
+		mail.setAttachmentFilePath("Path to file");
+		mail.setMailSubject("Spring Boot - Email Example");
+		mail.setMailContent("This is a test email");
+
+		ApplicationContext ctx = SpringApplication.run(EmailApplication.class, args);
+		MailService mailService = (MailService) ctx.getBean("mailService");
+		mailService.sendEmail(mail);
+
+	}
+
+}
---
+++ Email-Application/src/main/java/com/manas/email/services/MailService.java
@@ -0,0 +1,7 @@
+package com.manas.email.services;
+
+import com.manas.email.bean.Mail;
+
+public interface MailService {
+    public void sendEmail(Mail mail);
+}
